8d452d05eb80854c3b023dd46387bca74cbb1887
Mccranky83 <ericcai20021118@gmail.com>
2023-04-11 22:08:38 +0800

    Add several files

---
+++ lv1_2.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+# Level 1 -> 2 Prompt
+
+0 << 'END'
+The password for the next level is stored in a file called - located in the home directory
+END
+
+sshpass -p $(cat 0_1) ssh bandit1@bandit.labs.overthewire.org -p 2220
+
+# Note that the filename is '-', a reserved operator.
+cat ./-
+# rRGizSaX8Mk1RTb1CNQoXTcYZWU6lgzi
+
+exit
+
